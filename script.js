f42a0c0a4baf34239110f34e08821a81fd9d7df9
OILEGA <get2mena@gmail.com>
2024-02-13 13:25:42 +0100

    Add files via upload
    A web-based age calculator that calculates your age upon input of dob details

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculateAge() {
+    const dob = new Date(document.getElementById("dob").value);
+    const today = new Date();
+  
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+  
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
+  
+    document.getElementById("result").textContent = `Your age is: ${age}`;
+  }
+  
